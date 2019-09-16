75792b0a0a66b93e681e2fb679f0432873826455
Henrique Cunha Mantovani <henrique.m@nagro.app>
2019-09-16 15:58:02 -0300

    first commit

---
+++ src/database/index.js
@@ -0,0 +1,26 @@
+import Sequelize from 'sequelize';
+
+import User from '../app/models/User';
+import File from '../app/models/File';
+import databaseConfig from '../config/database';
+
+// Array com todos os models da app
+const models = [User, File];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  // Vai Fazer a conexão com a base de dados e carregar os Models
+  init() {
+    this.connection = new Sequelize(databaseConfig); // conexao com BD
+
+    // Percorrer o Array e passa a conexão para o Model
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
+  }
+}
+
+export default new Database();
---
+++ src/database/migrations/20190911023653-add-avatar-field-to-users.js
@@ -0,0 +1,16 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('users', 'avatar_id', {
+      type: Sequelize.INTEGER,
+      // td avatar_id da tabela users vai ser tb id contido na tabela files
+      references: { model: 'files', key: 'id' },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+      allowNull: true,
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.removeColumn('users', 'avatar_id');
+  },
+};
