3603835dba3764431294e028f54e81ae9d882b3d
Henrique Cunha Mantovani <henrique.m@nagro.app>
2020-01-16 23:05:47 -0300

    Validação de agendamento / Correções / .env

--- src/server.js
+++ src/server.js
@@ -1,3 +1,10 @@
 import app from './app';
 
+require('dotenv').config();
+
+const port = process.env.PORT;
+const hostname = process.env.HOST;
+
 app.listen(3333);
+
+console.log(`goBarber API Running on --> http://${hostname}:${port}`);
