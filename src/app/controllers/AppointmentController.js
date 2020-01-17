4bde2bfd4b52b5f841bde259f4f8ede5f888406a
Henrique Cunha Mantovani <henrique.m@nagro.app>
2020-01-16 23:33:19 -0300

    indes appointments

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -3,6 +3,7 @@ import { startOfHour, parseISO, isBefore } from 'date-fns';
 
 import Appointments from '../models/Appointments';
 import User from '../models/User';
+import File from '../models/File';
 
 class AppointmentController {
   async store(req, res) {
@@ -57,6 +58,30 @@ class AppointmentController {
     });
 
     return res.json(appointment);
+  }
+
+  async index(req, res) {
+    const appointments = await Appointments.findAll({
+      where: { user_id: req.userId, canceled_at: null },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+      ],
+    });
+
+    return res.status(200).json(appointments);
   }
 }
 
