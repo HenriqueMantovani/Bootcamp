7855e454f5fc302dcc3aabe0d684ae720a21855a
Henrique Cunha Mantovani <henrique.m@nagro.app>
2020-01-28 23:05:36 -0300

    feat: Configuração do Sentry

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import express from 'express';
+import 'express-async-errors';
+import Youch from 'youch';
 import path from 'path';
+import * as Sentry from '@sentry/node';
 import routes from './routes';
+import sentryConfig from './config/sentry';
 
 import './database';
 
@@ -8,12 +12,18 @@ class App {
   constructor() {
     this.server = express();
 
+    Sentry.init(sentryConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   // Onde vai cadastrar todos os middlewares
   middlewares() {
+    // Tem q ficar antes de todas as requisições
+    this.server.use(Sentry.Handlers.requestHandler());
+
     // Avisando o Express que vamos usar Json no corpo da requisição
     this.server.use(express.json());
 
@@ -26,6 +36,20 @@ class App {
 
   routes() {
     this.server.use(routes);
+
+    // The error handler must be before any other error middleware and after all controllers
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  // middleware de tratamento de exceção
+  exceptionHandler() {
+    // Express entende que quando um middleware recebe 4 parametros, é pq esse middleware
+    // para tratamento de execão
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
