8c13a710abac15269e0d854a8eafb53a9b62fc44
Henrique Cunha Mantovani <henrique.m@nagro.app>
2020-01-28 23:27:54 -0300

    feat: Configurando as variáveis de ambiente

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+import 'dotenv/config';
+
 import express from 'express';
 import 'express-async-errors';
 import Youch from 'youch';
@@ -46,9 +48,13 @@ class App {
     // Express entende que quando um middleware recebe 4 parametros, é pq esse middleware
     // para tratamento de execão
     this.server.use(async (err, req, res, next) => {
-      const errors = await new Youch(err, req).toJSON();
+      if (process.env.NODE_ENV === 'developments') {
+        const errors = await new Youch(err, req).toJSON();
 
-      return res.status(500).json(errors);
+        return res.status(500).json(errors);
+      }
+
+      return res.status(500).json({ error: 'Internal server error' });
     });
   }
 }
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://0fd310c12f3a4c9fb37bd79be37c32ed@sentry.io/2052955',
+  dsn: process.env.SENTRY_DSN,
 };
